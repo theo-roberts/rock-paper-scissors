d60b972dffbff00001865ff0f7e7ba703d82cb3f
Theo Roberts <theo10roberts@gmail.com>
2023-08-02 11:21:51 +0100

    made game

--- scripts.js
+++ scripts.js
@@ -1,10 +1,40 @@
+
+let playerScore = 0;
+let computerScore = 0;
+const array = ['rock', 'paper', 'scissors'];
+let playerChoice = "choice";
+let computerChoice = "choice"
+
 function getComputerChoice(arr) {
     const randomIndex = Math.floor(Math.random() * arr.length);
     const item = arr[randomIndex];
     return item;
 }
 
-const array = ['Rock', 'Paper', 'Scissors'];
+computerChoice = getComputerChoice(array);
+playerChoice = prompt ("type rock, paper or scissors").toLowerCase();
 
-const result = getComputerChoice(array);
-console.log(result);
+function playRound(){
+    if (playerChoice == computerChoice){
+        return("It's a Draw!")
+    }
+    if (
+        (playerChoice == 'rock' && computerChoice == 'scissors' ||
+        playerChoice == 'paper' && computerChoice == 'rock' ||
+        playerChoice == 'scissors' && computerChoice == 'paper')
+    ){
+        playerScore = playerScore +1
+        return ("You Win! " + playerChoice + " beats " + computerChoice + "!")
+    }
+    if (
+        (playerChoice == 'rock' && computerChoice == 'paper' ||
+        playerChoice == 'paper' && computerChoice == 'scissors' ||
+        playerChoice == 'scissors' && computerChoice == 'rock')
+    ){
+        computerScore = computerScore + 1
+        return("You Lose :( " + computerChoice + " beats " + playerChoice + "!")
+    }
+}
+
+
+console.log(playRound(), playerScore, computerScore)
