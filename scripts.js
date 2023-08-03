e7b86d35ff35b145a8a2b9f2ec951fbd3abc8db7
Theo Roberts <theo10roberts@gmail.com>
2023-08-03 16:09:35 +0100

    make 5 round game

--- scripts.js
+++ scripts.js
@@ -4,6 +4,7 @@ let computerScore = 0;
 const array = ['rock', 'paper', 'scissors'];
 let playerChoice = "choice";
 let computerChoice = "choice"
+let score = ""
 
 function getComputerChoice(arr) {
     const randomIndex = Math.floor(Math.random() * arr.length);
@@ -11,30 +12,51 @@ function getComputerChoice(arr) {
     return item;
 }
 
-computerChoice = getComputerChoice(array);
-playerChoice = prompt ("type rock, paper or scissors").toLowerCase();
-
-function playRound(){
-    if (playerChoice == computerChoice){
-        return("It's a Draw!")
+function delcareWinner() {
+    if (playerScore == computerScore){
+        alert ("It's a Draw!!!")
+        }
+    else if (playerScore > computerScore){
+        alert ("You Win!!!!!")
     }
-    if (
-        (playerChoice == 'rock' && computerChoice == 'scissors' ||
-        playerChoice == 'paper' && computerChoice == 'rock' ||
-        playerChoice == 'scissors' && computerChoice == 'paper')
-    ){
-        playerScore = playerScore +1
-        return ("You Win! " + playerChoice + " beats " + computerChoice + "!")
-    }
-    if (
-        (playerChoice == 'rock' && computerChoice == 'paper' ||
-        playerChoice == 'paper' && computerChoice == 'scissors' ||
-        playerChoice == 'scissors' && computerChoice == 'rock')
-    ){
-        computerScore = computerScore + 1
-        return("You Lose :( " + computerChoice + " beats " + playerChoice + "!")
+    else if (playerScore < computerScore){
+        alert ("Computer Wins :(")
     }
 }
 
+function playGame(){
+    for (let i = 0; i < 5; i++){
+        computerChoice = getComputerChoice(array);
+        playerChoice = prompt ("type rock, paper or scissors").toLowerCase();
+        playRound();
+        score = ("You: " + playerScore + " " + "Computer: " + computerScore);
+        }
+        delcareWinner()    
+}
 
-console.log(playRound(), playerScore, computerScore)
+function playRound(){
+    if (
+        playerChoice == computerChoice)
+        alert("Draw")
+    
+    else if (
+        playerChoice == 'rock' && computerChoice == 'scissors' ||
+        playerChoice == 'paper' && computerChoice == 'rock' ||
+        playerChoice == 'scissors' && computerChoice == 'paper')
+        {
+        alert ("You Win! " + playerChoice + " beats " + computerChoice + "!")
+        ++playerScore
+        }
+    else if (
+        playerChoice == 'rock' && computerChoice == 'paper' ||
+        playerChoice == 'paper' && computerChoice == 'scissors' ||
+        playerChoice == 'scissors' && computerChoice == 'rock')
+        {
+        alert ("You Lose :( " + computerChoice + " beats " + playerChoice + "!")
+        ++computerScore
+        }
+}
+
+
+
+console.log(playGame(), score)
